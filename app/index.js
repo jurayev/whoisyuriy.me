4790d076e4b905d4fd006c6c287b78b1b908c563
Yuriy Jurayev <jurayevy@ryanair.com>
2019-09-16 23:35:15 +0200

    create pre-prod source version

--- app/index.js
+++ app/index.js
@@ -6,12 +6,6 @@ import 'jquery';
 import 'jquery.easing';
 
 import 'js/typewriter';
-//import 'js/particles';
 import 'js/scrolling';
 import 'js/animation';
 import 'js/preloader'
-
-// adds all custom Bootstrap jQuery plugins
-// see all plugins here: http://getbootstrap.com/javascript/
-
-
